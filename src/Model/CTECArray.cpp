4e535c9ea175963202795d1d73b275fb4b401957
meanmonkey662 <coolkoch8535@gmail.com>
2016-02-04 12:19:12 -0700

    Added array

--- src/Model/CTECArray.cpp
+++ src/Model/CTECArray.cpp
@@ -6,13 +6,88 @@
  */
 
 #include "CTECArray.h"
+using namespace std;
 
-CTECArray::CTECArray() {
-	// TODO Auto-generated constructor stub
+template <class Type>
+CTECArray<Type>::CTECArray(int size)
+{
+	this->size = size;
+	this->head = nullptr;
 
+	//Defensive
+	if(size <=0)
+	{
+		cerr << "That is not valid"
+		return;
+	}
 }
 
-CTECArray::~CTECArray() {
+template <class Type>
+CTECArray<Type>::~CTECArray()
+{
 	// TODO Auto-generated destructor stub
 }
 
+template <class Type>
+int CTECArray<Type> :: getSize()
+{
+	return this->size;
+}
+
+template <class Type>
+Type* CTECArray<Type> :: get(int position)
+{
+	//We need to do bounds checking so we do not crash the program.
+	if(position >= size || position < 0)
+	{
+		//I am out of bounds and need to do something about it.
+		cerr << "position value is out of bounds :(" << endl;
+		return nullptr;
+	}
+	else
+	{
+		//I am in bounds so I am inclusive
+		ArrayNode<Type> * current = head;
+		for(int spot = 0; spot <= position; spot++)
+		{
+			if(spot != position)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+				return current->getValue();
+			}
+		}
+	}
+}
+
+template <class Type>
+void CTECArray<Type>::set(int position, Type value)
+{
+	if (position >= size || position < 0)
+	{
+		//I am out of bounds and need to do something about it.
+		cerr << "position value is out of bounds :(" << endl;
+	}
+	else
+	{
+		ArrayNode<Type> * current = head;
+		for (int spot = 0; spot < position; spot++)
+		{
+			if (spot != position)
+			{
+				current = current->getNext();
+			}
+			else
+			{
+				current->setValue(value);
+			}
+		}
+	}
+
+template <class Type>
+void CTECArray<Type> :: set(int position, Type value)
+{
+
+}
